96d3f1d633fb32fdc0a17f6aa12e387b319fb751
Aradhya <arutripathi011@gmail.com>
2022-05-12 15:19:36 +0530

    Route fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <div className='navbar flex-row'>
-      <span className='text'> <Link to='/'>All banks</Link> </span>
+      <span className='text'> <Link to='/FindBanks'>All banks</Link> </span>
       <span className='text'><Link to="/favourites">Favourites</Link> </span>
     </div>
   )
